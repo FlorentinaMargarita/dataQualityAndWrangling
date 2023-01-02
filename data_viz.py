0e962884fed0fcc4e66e34d6c70332cd83353024
FloTheFlower <florentina.hofbauer@gmail.com>
2023-01-02 15:44:30 -0800

    cleaning the data

--- data_viz.py
+++ data_viz.py
@@ -11,11 +11,83 @@ np.set_printoptions(suppress=True)
 
 hf = h5py.File("records.h5", "r")
 
-with h5py.File('records.h5', 'r') as filinger:
-    lucid_check = filinger.get('2022-11-30T21')
-    make_array = lucid_check[...]
-    print(np.array(lucid_check), 'lucid_check')
+all_gasprices = []
+megawatthours_demanded = []
+all_tesla_data = []
+all_rivian_data = []
+all_lucid_data = []
 
 all_dates = list(hf.keys())
 
-print(all_dates, 'alldates')
+all_data = []
+
+time_line_dates  = []
+clean_dates = [x[0:10] for x in all_dates]
+unique_dates = []
+for x in clean_dates: 
+    if x not in unique_dates:
+        unique_dates.append(x)
+
+used_dates = []
+#cleaning data to get only one row of values per day.
+with h5py.File('records.h5', 'r') as filinger:
+    for date in all_dates:
+        formatted_date = date[0:10]
+        if formatted_date not in used_dates:
+            lucid_check = filinger.get(date)
+            make_array = lucid_check[...]
+            all_data.append({formatted_date: make_array})
+            used_dates.append(formatted_date)
+
+
+print(all_data, 'all_data')
+print(used_dates, 'used_dates')
+
+
+
+
+
+for data in all_data:
+
+        all_gasprices.append(make_array[0][0])
+        megawatthours_demanded.append(make_array[0][1])
+        all_tesla_data.append(make_array[0][2])
+        all_rivian_data.append(make_array[0][3])
+        all_lucid_data.append(make_array[0][4])
+
+#     print(np.array(lucid_check), 'lucid_check')
+
+
+print(len(all_dates), 'alldates')
+print(len(all_gasprices), 'all_gasprices')
+
+
+df = pd.DataFrame({'Date': used_dates,
+                     
+                   'Gasprice': all_gasprices,
+                     
+                   'Megawatth': megawatthours_demanded,
+                     
+                   'Tesla': all_tesla_data,
+                   'Rivian': all_rivian_data,
+                   'Lucid': all_lucid_data})
+
+# g = sns.lineplot(x = "Date", y = "Tesla",
+#              data = df)
+
+# g = sns.lineplot(x = "Date", y = "Rivian",
+#              data = df)
+
+# g = sns.lineplot(x = "Date", y = "Lucid",
+#              data = df)
+
+
+# g = sns.tsplot(x = "Date", y = "Megawatth",
+#              data = df)
+
+# g = sns.lineplot(x = "Date", y = "Gasprice",
+            #  data = df)
+
+# plt.xticks(rotation = 25)
+df.plot()
+plt.show()
